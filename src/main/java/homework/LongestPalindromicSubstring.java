e0bdf11e163de191bf6db816516a5e5ae59e8102
kjcKen <2145988604@qq.com>
2025-08-10 10:07:54 +0800

    remove answer

--- src/main/java/homework/LongestPalindromicSubstring.java
+++ src/main/java/homework/LongestPalindromicSubstring.java
@@ -5,18 +5,7 @@ import java.util.Scanner;
 public class LongestPalindromicSubstring {
     public static String longestPalindrome(String s) {
         // TODO begin
-        if (s == null || s.length() < 1) return "";
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);     // 奇数长度
-            int len2 = expandAroundCenter(s, i, i + 1); // 偶数长度
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
-        }
-        return s.substring(start, end + 1);
+
         // TODO end
     }
 
